c0f2fbbe21efeb3e6fed2f0017d9b8434f7cb2b6
virag-ky <vkormoczy@gmail.com>
2024-01-27 21:03:42 +0200

    Add border to collection when selected

--- Day1/index.js
+++ Day1/index.js
@@ -15,12 +15,16 @@ collections.forEach(collection => {
   collection.addEventListener('click', (e) => {
 
     [...document.querySelectorAll('.check')].forEach(check => check.remove());
+    collections.forEach(c => {
+      c.style.borderColor = "transparent"
+  })
    
     const checkmark = document.createElement('div');
     checkmark.setAttribute("class", "check");
     checkmark.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="40" height="40" viewBox="0 0 24 24" stroke-width="2.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>`;
 
     collection.appendChild(checkmark);
+    collection.style.borderColor = "#f0f0ff";
   })
 });
 
@@ -29,5 +33,8 @@ collections.forEach(collection => {
 document.body.addEventListener('click', (e) => {
   if (!e.target.closest('.collection')) {
     [...document.querySelectorAll('.check')].forEach(check => check.remove());
+    collections.forEach(c => {
+      c.style.borderColor = "transparent"
+  })
   }
 })
